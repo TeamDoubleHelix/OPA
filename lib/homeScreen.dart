a0e8d393fe7f92974a2419ae13bce023cc0ec507
liamdratta <liam.dratta@gmail.com>
2019-03-09 19:23:34 +0100

    last one before sleep

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -1,18 +1,37 @@
 
-
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:opa/Friends.dart';
 import 'package:opa/Time.dart';
 import 'package:opa/button.dart';
 
 import 'package:opa/sosPage.dart';
+import 'package:tts/tts.dart';
 
-class HomeScreen extends StatelessWidget {
+
+class HomeScreen extends StatefulWidget {
 HomeScreen(this.name);
 final String name;
   @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+
+
+speak() async {
+  setState(() {
+      Tts.speak("a");
+  });
+
+}
+
+
+
+  @override
   Widget build(BuildContext context) {
-    var dateTime = new DateTime.now();
+
     return Scaffold(
       body: Column(
         children: <Widget>[
@@ -22,7 +41,7 @@ final String name;
                 Padding(
                   padding: const EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 10.0),
                   child: Text(
-                    "Merħba $name",
+                    "Merħba ${widget.name}",
                     textAlign: TextAlign.center,
                     style: Theme.of(context).textTheme.body2,
                     textScaleFactor: 3.7,
@@ -58,10 +77,14 @@ final String name;
             crossAxisCount: 2,
             childAspectRatio: 0.8,
             children: <Widget>[
+              
+             
+
               MyButton(
-                  Icons.event_available, Colors.teal, 'Events', false, () {}),
+                
+                  Icons.event_available, Colors.teal, 'Events', false, speak,),
               MyButton(
-                  Icons.smoking_rooms, Colors.blue, 'Puzzles', false, () {}),
+                Icons.perm_media, Colors.blue, 'Puzzles', false, () {}),
               MyButton(Icons.people, Colors.orange, 'Friends',
                   false, () {
                      Navigator.push(context, MaterialPageRoute( builder: (context)=> Friends()));
@@ -76,3 +99,4 @@ final String name;
     );
   }
 }
+
